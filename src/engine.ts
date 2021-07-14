c60eb7cc12921796b8220c5320a8f856eaf68817
aimenin <artem.igorevich8@rambler.ru>
2021-07-14 19:25:19 +0300

    Добавит операцию pow

--- src/engine.ts
+++ src/engine.ts
@@ -6,7 +6,7 @@ import {
   mathOperatorsPriorities,
 } from "./mathOperators";
 
-const { FIRST, SECOND } = mathPriorities;
+const [FIRST, SECOND, THIRD] = mathPriorities;
 
 export const firstPrioritiesCalc = (stack: ParsedLineType): ParsedLineType => {
   let result: ParsedLineType = [];
@@ -29,20 +29,38 @@ export const firstPrioritiesCalc = (stack: ParsedLineType): ParsedLineType => {
   return result;
 };
 
-export const secondPrioritiesCalc = (stack: ParsedLineType): number => {
-  let result = 0;
-  for (let key = 0; key < stack.length; key++) {
-    if (key === 0) {
-      result = Number(stack[key]);
-    }
-
-    const prevItem = result;
-    const item = stack[key - 1];
-    const nextItem = stack[key];
+export const secondPrioritiesCalc = (stack: ParsedLineType): ParsedLineType =>
+  stack.reduce<ParsedLineType>((result, nextItem) => {
+    const prevItem = result[result.length - 2];
+    const item = result[result.length - 1];
 
     if (!isNumber(String(item)) && mathOperatorsPriorities[item] === SECOND) {
-      result = mathOperators[item](Number(prevItem), Number(nextItem));
+      if (!mathOperators[item]) {
+        throw new TypeError("Unexpected stack!");
+      }
+      result = [
+        ...result.slice(0, -2),
+        mathOperators[item](Number(prevItem), Number(nextItem)),
+      ];
+    } else {
+      result.push(nextItem);
     }
-  }
-  return result;
-};
+    return result;
+  }, []);
+
+export const thirdPrioritiesCalc = (stack: ParsedLineType): number =>
+  stack.reduce<number>((result, nextItem, key) => {
+    const item = stack[key - 1];
+
+    if (
+      mathOperatorsPriorities[item] === FIRST ||
+      mathOperatorsPriorities[item] === SECOND
+    ) {
+      throw new TypeError("Unexpected stack!");
+    }
+
+    if (!isNumber(String(item)) && mathOperatorsPriorities[item] === THIRD) {
+      result = mathOperators[item](Number(result), Number(nextItem));
+    }
+    return result;
+  }, Number(stack[0]));
--- src/mathOperators.test.ts
+++ src/mathOperators.test.ts
@@ -1,25 +1,37 @@
-import { mul, div, add, minus } from "./mathOperators";
+import { mul, div, add, minus, pow } from "./mathOperators";
 
-test("mul 1 * 2 to equal 2", () => {
-  expect(mul(1, 2)).toBe(2);
+describe("Standart functions", () => {
+  it("mul 1 * 2 to equal 2", () => {
+    expect(mul(1, 2)).toBe(2);
+  });
+
+  it("mul 1 * 2 to equal 2", () => {
+    expect(mul(1, 2)).toBe(2);
+  });
+
+  it("mul 2 * 2 to equal 4", () => {
+    expect(mul(2, 2)).toBe(4);
+  });
+
+  it("div 2 / 2 to equal 1", () => {
+    expect(div(2, 2)).toBe(1);
+  });
+
+  it("div 4 / 2 to equal 2", () => {
+    expect(div(4, 2)).toBe(2);
+  });
+
+  it("add 4 + 2 to equal 6", () => {
+    expect(add(4, 2)).toBe(6);
+  });
+
+  test("minus 4 - 2 to equal 2", () => {
+    expect(minus(4, 2)).toBe(2);
+  });
 });
 
-test("mul 2 * 2 to equal 4", () => {
-  expect(mul(2, 2)).toBe(4);
-});
-
-test("div 2 / 2 to equal 1", () => {
-  expect(div(2, 2)).toBe(1);
-});
-
-test("div 4 / 2 to equal 2", () => {
-  expect(div(4, 2)).toBe(2);
-});
-
-test("add 4 + 2 to equal 6", () => {
-  expect(add(4, 2)).toBe(6);
-});
-
-test("minus 4 - 2 to equal 2", () => {
-  expect(minus(4, 2)).toBe(2);
+describe("addictional functionality", () => {
+  it("pow 3 ^ 2 to equal 9", () => {
+    expect(pow(3, 2)).toBe(9);
+  });
 });
